6fff35d390314dc4960b99d5dd831d8bd3ab8c30
AhMeD_JuTt <109766483+AhMeD-JuTtt@users.noreply.github.com>
2023-02-14 23:30:47 +0500

    Question 1 to 14

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-let a = 30;
-console.log("the number is", a);
+// 1. Write a js program to find the maximum between two numbers.
+
+let a = 10;
+let b = 20;
+let c;
+
+if (a > b) {
+    c = a;
+} else {
+    c = b;
+}
+
+console.log("The maximum of two numbers is", c);
